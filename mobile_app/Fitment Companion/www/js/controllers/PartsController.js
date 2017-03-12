dc93027c8e22ded27b4561e488f3586c3b3f7085
NimbusDev <31594674+StillAliveDev@users.noreply.github.com>
2017-03-12 19:08:34 +0000

    Added the remainder of the Modals, Various Modifications to file in css, js and templates. New templates added to support the modals.

--- mobile_app/Fitment Companion/www/js/controllers/PartsController.js
+++ mobile_app/Fitment Companion/www/js/controllers/PartsController.js
@@ -1,9 +1,27 @@
 (function(){
 	angular.module('starter')
-	.controller('PartsController',['$scope','$state','localStorageService','SocketService', PartsController]);
+	.controller('PartsController',['$scope','$state','$ionicPopup','localStorageService','SocketService', PartsController]);
 	
-	function PartsController($scope,$state,localStorageService,SocketService){
+	function PartsController($scope,$state,$ionicPopup,localStorageService,SocketService){
 		var me = this;
+		
+		$scope.openPart = function(part){
+			console.log('Opening Part Popup');
+			var partPopup = $ionicPopup.confirm({
+				title: 'Part Detail',
+				templateUrl: 'templates/partDetailModal.html',
+				scope: $scope
+			});
+		};
+		
+		$scope.openPartScan = function(){
+			console.log('Opening Part Scan Popup');
+			var partPopup = $ionicPopup.confirm({
+				title: 'Scan Tag',
+				templateUrl: 'templates/partScanModal.html',
+				scope: $scope
+			});
+		};
 	}
 	
 })();
--- mobile_app/Fitment Companion/www/js/controllers/VehiclesController.js
+++ mobile_app/Fitment Companion/www/js/controllers/VehiclesController.js
@@ -1,9 +1,59 @@
 (function(){
 	angular.module('starter')
-	.controller('VehiclesController',['$scope','$state','localStorageService','SocketService', VehiclesController]);
+	.controller('VehiclesController',['$scope','$state','$ionicPopup','localStorageService','SocketService', VehiclesController]);
 	
-	function VehiclesController($scope,$state,localStorageService,SocketService){
+	function VehiclesController($scope,$state,$ionicPopup,localStorageService,SocketService){
 		var me = this;
+		$scope.openVIN = function(vin){
+			console.log('Opening Vehicle Detail Popup');
+			var vehiclePopup = $ionicPopup.confirm({
+				title: 'Selected Vehicle',
+				templateUrl: 'templates/vehicleDetailModal.html',
+				scope: $scope
+			});
+		};
+		
+		$scope.openGroup = function(group){
+			console.log('Opening Group Detail Popup');
+			var groupPopup = $ionicPopup.confirm({
+				title: 'Fitment Detail',
+				templateUrl: 'templates/fitmentGroupDetailModal.html',
+				scope: $scope,
+				okText: 'Start'
+			});
+		};
+		
+		$scope.openPart = function(part){
+			console.log('Opening Part Popup');
+			var partPopup = $ionicPopup.confirm({
+				title: 'Part Detail',
+				templateUrl: 'templates/partDetailModal.html',
+				scope: $scope
+			});
+		};
+		
+		$scope.openPartScan = function(){
+			console.log('Opening Part Scan Popup');
+			var partPopup = $ionicPopup.confirm({
+				title: 'Scan Tag',
+				templateUrl: 'templates/partScanModal.html',
+				scope: $scope
+			});
+		};		
+		
+		$scope.openVehicleScan = function(){
+			console.log('Opening Vehicle Scan Popup');
+			var partPopup = $ionicPopup.confirm({
+				title: 'Scan Tag',
+				templateUrl: 'templates/vehicleScanModal.html',
+				scope: $scope
+			});
+		};
+		
+		$scope.randomPrint = function(){
+			console.log('hello world');
+		};
+		
 	}
 	
 })();
