5d753dff423efb6fc94a4eb61517d8c9153a8835
NimbusDev <31594674+StillAliveDev@users.noreply.github.com>
2017-03-27 14:43:16 +0000

    Added home screen database access, and real-time responses for when desktop app is developed.
    -- Left out server.js --- No included in this commit

--- serverside/Fitment_Companion_Serverside/server.js
+++ serverside/Fitment_Companion_Serverside/server.js
@@ -3,11 +3,11 @@ var io = require('socket.io')(6100);
 var loginFunc = require('./js/login.js');
 var vehFunc = require('./js/vehicle.js');
 var partsFunc = require('./js/part.js');
+var homeFunc = require('./js/home.js');
 
 io.on('connection', function(socket){
-    //Handles Login Nroadcasts
+    //Handles Login broadcasts
     socket.on('user:login', function (data){
-
         loginFunc.doLogin(data, function(err,content){
            if(err){
                console.log(err);
@@ -15,7 +15,9 @@ io.on('connection', function(socket){
            }
            else{
                console.log(content);
+               io.sockets.emit('login:login');
                socket.emit('login:success', content);
+
            }
         });
     });
@@ -23,7 +25,15 @@ io.on('connection', function(socket){
     //Handles logout broadcasts
     socket.on('user:logout', function(data){
         loginFunc.doLogout(data, function(err,content){
-
+        if(err){
+            console.log(err);
+            socket.emit('logout:fail');
+        }
+        else{
+            console.log(content);
+            io.sockets.emit('logout:logout');
+            socket.emit('logout:success');
+        }
         });
     });
 
@@ -42,6 +52,7 @@ io.on('connection', function(socket){
         });
     });
 
+    //Handles response when user refreshes the parts list
     socket.on('parts:loadList',function(){
         console.log('Request to reload Parts List Received');
         partsFunc.loadAllParts(function(err,content){
@@ -52,6 +63,22 @@ io.on('connection', function(socket){
             else{
                 console.log(content);
                 socket.emit('parts:loadComplete', content);
+            }
+        });
+    });
+
+    //Handles response for mobile application home screen
+    socket.on('home:load', function(){
+        console.log('Request to load home screen data');
+
+        homeFunc.loadHomeData(function(err, content){
+            if(err){
+                console.log(err);
+                socket.emit('home:loadError', err);
+            }
+            else{
+                console.log(content);
+                socket.emit('home:loadSuccess', content);
             }
         });
     });
