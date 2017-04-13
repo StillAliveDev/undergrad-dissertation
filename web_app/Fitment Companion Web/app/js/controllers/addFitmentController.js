c6449e72243ff70e6318e60c10e4a5ef9ea5e03d
NimbusDev <31594674+StillAliveDev@users.noreply.github.com>
2017-04-13 12:08:49 +0000

    Added missing addfitments controller and template

---
+++ web_app/Fitment Companion Web/app/js/controllers/addFitmentController.js
@@ -0,0 +1,57 @@
+angular.module('myApp.addFitment', ['ngRoute', 'ui.bootstrap'])
+
+    .config(['$routeProvider', function($routeProvider){
+        $routeProvider.when('/fitments/add', {
+            templateUrl: 'templates/addFitments.html',
+            controller: 'AddFitmentsController'
+        });
+    }])
+    .controller('AddFitmentsController', function($location, $window, $rootScope, $scope, SocketService) {
+        $scope.currentUser = {
+            id: $window.sessionStorage.user_id,
+            user_name:$window.sessionStorage.user_name
+        };
+
+        $scope.controllerData = {
+
+        };
+
+        /*Controller Functions*/
+
+
+
+        /*Navbar Functions*/
+        $scope.logout = function(){
+            var data = {
+                userid : $scope.currentUser.id,
+                username: $scope.currentUser.user_name
+            };
+            SocketService.emit('user:logout', angular.toJson(data));
+        };
+
+        SocketService.on('logout:success', function(){
+            $window.sessionStorage.user_id = {};
+            $window.sessionStorage.user_name = {};
+            $location.path('/login');
+        });
+
+        $scope.navVehicles = function(){
+            $location.path('/vehicles');
+        };
+
+        $scope.navUsers = function(){
+            $location.path('/users');
+        };
+
+        $scope.navParts = function(){
+            $location.path('/parts');
+        };
+
+        $scope.navFitments = function(){
+            $location.path('/fitments');
+        };
+
+        $scope.navHome = function() {
+            $location.path('/home');
+        };
+    });
