f33345d413703f6185c964ae7c81f20b48f307fa
NimbusDev <31594674+StillAliveDev@users.noreply.github.com>
2017-04-14 19:06:37 +0000

    More documentation for templates

--- mobile_app/Fitment Companion/www/js/services/SocketService.js
+++ mobile_app/Fitment Companion/www/js/services/SocketService.js
@@ -13,6 +13,8 @@
 		return socketFactory({
 			ioSocket: io.connect('http://192.168.1.5:6100') //IP Address.
 			//Port 6100 is arbitrary, but must be the same on the serverside.
+			//This IP Address will change depening on the IP address of the serverside... Check before deployment
+			//Reflect this IP in index too.
 		});
 	}
 })();
